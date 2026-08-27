9c5a1c14daa3158bdb878ec8021a8aa0d0d21475
Abylay Makhmetzhan <ablai96@gmail.com>
2026-08-27 20:07:08 +0500

    feat(ui): disable default browser context menu in production builds

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 
+// Disable default browser context menu in production / release builds
+if (import.meta.env.PROD) {
+  document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <HashRouter>
@@ -10,3 +17,4 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     </HashRouter>
   </React.StrictMode>,
 );
+
